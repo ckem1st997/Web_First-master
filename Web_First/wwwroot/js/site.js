d099e9c158fce0bc40f0a327ba8cc3338876642e
NGUYEN KHA HOP <1721050681@student.humg.edu.vn>
2020-06-17 08:29:16 +0700

    17/6/2020

--- Web_First/wwwroot/js/site.js
+++ Web_First/wwwroot/js/site.js
@@ -506,10 +506,10 @@ $(document).ready(function () {
             }
         }
     });
-    for (var i = 0; i < 609; i++) {
-        if ($(".total")[i].innerText.length == 0)
-            $(".total")[i].append("1000 sản phẩm của 1 loại");
-    }
+    //for (var i = 0; i < 609; i++) {
+    //    if ($(".total")[i].innerText.length == 0)
+    //        $(".total")[i].append("1000 sản phẩm của 1 loại");
+    //}
 });
 
 
